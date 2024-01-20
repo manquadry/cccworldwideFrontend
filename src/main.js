694e2821ef71c0c59cd278783f91346b159054c6
Quadry <65723498+manquadry@users.noreply.github.com>
2024-01-20 15:30:21 +0100

    main changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-/* eslint-disable import/order */
+
 import '@/@fake-db/db'
 import '@/@iconify/icons-bundle'
 import App from '@/App.vue'
@@ -11,9 +11,19 @@ import router from '@/router'
 import { abilitiesPlugin } from '@casl/vue'
 import '@core/scss/template/index.scss'
 import '@styles/styles.scss'
+import 'flag-icons/css/flag-icons.min.css'
 import { createPinia } from 'pinia'
+import { createVPhoneInput } from 'v-phone-input'
+import 'v-phone-input/dist/v-phone-input.css'
 import { createApp } from 'vue'
+import vueCountryRegionSelect from 'vue3-country-region-select'
+import CountryFlag from 'vue-country-flag-next'
 
+
+const vPhoneInput = createVPhoneInput({
+  countryIconMode: 'svg',
+  
+})
 
 
 loadFonts()
@@ -22,10 +32,12 @@ loadFonts()
 // Create vue app
 const app = createApp(App)
 
+app.use(vueCountryRegionSelect)
+app.component('CountryFlag', CountryFlag)
 
 // Use plugins
 app.use(vuetify)
-
+app.use(vPhoneInput)
 app.use(createPinia())
 app.use(router)
 app.use(layoutsPlugin)
